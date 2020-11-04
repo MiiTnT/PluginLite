78843b0ef9cc25442564175aa54b05296281558d
Benco11 <65251723+Benco11-developement@users.noreply.github.com>
2020-11-04 18:17:25 +0100

    Update and rename furnace.java to CommandFurnace.java

--- src/main/java/fr/miitnt/pluginlite/commands/CommandFurnace.java
+++ src/main/java/fr/miitnt/pluginlite/commands/CommandFurnace.java
@@ -1,7 +1,9 @@
 package fr.miitnt.pluginlite.commands;
 
 import fr.miitnt.pluginlite.Pluginlite;
+
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,19 +11,18 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 
-public class furnace implements CommandExecutor {
-   private Pluginlite main;
+public class CommandFurnace implements CommandExecutor {
+    private Pluginlite main;
 
-    public furnace(Pluginlite main) {
+    public CommandFurnace(Pluginlite main) {
         this.main = main;
     }
-
-
+   
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player player = (Player)sender;
         if(sender instanceof Player){
-            player.sendMessage("§4§lNE FERMEZ PAS CE FOUR AVANT D'AVOIR TOUT RECUPERER SOUS PEINE DE TOUT PERDRE !");
+            player.sendMessage(ChatColor.DARK_RED + "" + ChatColor.BOLD + "NE FERMEZ PAS CE FOUR AVANT D'AVOIR TOUT RECUPERER SOUS PEINE DE TOUT PERDRE !");
         }
         Bukkit.getScheduler().runTaskLater(main, new Runnable() {
             @Override
