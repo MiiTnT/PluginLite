7b4574771936cbf72b14fe99431dbce08c77bd01
Benco11 <65251723+Benco11-developement@users.noreply.github.com>
2020-11-04 17:50:45 +0100

    Update PlayersListener.java

--- src/main/java/fr/miitnt/pluginlite/listeners/PlayersListener.java
+++ src/main/java/fr/miitnt/pluginlite/listeners/PlayersListener.java
@@ -13,8 +13,10 @@ import java.util.List;
 public class PlayersListener implements Listener {
 
     private List<String> forbiddenCommands = Arrays.asList("plugins", "pl", "ver", "version", "help", "?");
-
+    private Pluginlite main;
+    
     public PlayersListener(Pluginlite pluginlite) {
+        this.main = pluginlite;
     }
 
     @EventHandler
@@ -23,6 +25,70 @@ public class PlayersListener implements Listener {
         if (forbiddenCommands.contains(e.getMessage().toLowerCase().replace("/", "").replaceAll(" ",""))) {
             p.sendMessage("§cErreur: §7Cette commande a été désactivée par §2[PluginLite]");
             e.setCancelled(true);
+        }
+    }
+    
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Player p = event.getPlayer();
+        String message = this.main.getConfig().getString("messages.join_message");
+        if (message != null)
+            event.setJoinMessage(message.replace("{PLAYER}", p.getDisplayName()));
+        
+        if(p.hasPermission("tab.prefix.joueur")){
+            p.setPlayerListName(ChatColor.DARK_GREEN + "{J} " + p.getName());
+        }
+        if(p.hasPermission("tab.prefix.guerrier")){
+            p.setPlayerListName(ChatColor.RED + "{G} " + p.getName());
+        }
+        if(p.hasPermission("tab.prefix.admin")){
+            p.setPlayerListName(ChatColor.DARK_RED + "{A} " + p.getName());
+        }
+        if(p.hasPermission("tab.prefix.moderateur")){
+            p.setPlayerListName(ChatColor.DARK_GREEN + "{M} " + p.getName());
+        }
+        if(p.hasPermission("tab.prefix.helper")){
+            p.setPlayerListName(ChatColor.LIGHT_PURPLE + "{H} " + p.getName());
+        }
+        if(p.hasPermission("tab.prefix.responsable")){
+            p.setPlayerListName(ChatColor.BLUE + "{R} " + p.getName());
+        }
+        if(p.hasPermission("tab.prefix.builder")){
+            p.setPlayerListName(ChatColor.GOLD + "{B} " + p.getName());
+        }
+
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+        Player p = event.getPlayer();
+        String message = this.main.getConfig().getString("messages.quit_message");
+
+        if (message != null)
+            event.setQuitMessage(message.replace("{PLAYER}", p.getDisplayName()));
+    }
+
+    @EventHandler
+    public void onChat(AsyncPlayerChatEvent event) {
+        Player p = event.getPlayer();
+        String message = event.getMessage();
+        if (p.hasPermission("chat.format.joueur")) {
+            event.setFormat(ChatColor.DARK_GRAY + "[" + ChatColor.DARK_GREEN + "Joueur" + ChatColor.DARK_GRAY + "] " + ChatColor.DARK_GREEN + p.getDisplayName() + ChatColor.DARK_GRAY + ": " + ChatColor.WHITE + message);
+        } else if (p.hasPermission("chat.format.guerrier")) {
+            event.setFormat(ChatColor.DARK_GRAY + "[" + ChatColor.RED + "Guerrier" + ChatColor.DARK_GRAY + "] " + ChatColor.RED + p.getDisplayName() + ChatColor.DARK_GRAY + ": " + ChatColor.WHITE + message);
+            //This will give the player with that permission node that Chat format.
+        } else if (p.hasPermission("chat.format.admin")) {
+            event.setFormat(ChatColor.DARK_GRAY + "[" + ChatColor.DARK_RED + "Admin" + ChatColor.DARK_GRAY + "] " + ChatColor.DARK_RED + p.getDisplayName() + ChatColor.DARK_GRAY + ": " + ChatColor.WHITE + message);
+        } else if (p.hasPermission("chat.format.moderateur")) {
+            event.setFormat(ChatColor.DARK_GRAY + "[" + ChatColor.GRAY + "Modérateur" + ChatColor.DARK_GRAY + "] " + ChatColor.GRAY + p.getDisplayName() + ChatColor.DARK_GRAY + ": " + ChatColor.WHITE + message);
+        } else if (p.hasPermission("chat.format.helper")) {
+            event.setFormat(ChatColor.DARK_GRAY + "[" + ChatColor.LIGHT_PURPLE + "Helper" + ChatColor.DARK_GRAY + "] " + ChatColor.LIGHT_PURPLE + p.getDisplayName() + ChatColor.DARK_GRAY + ": " + ChatColor.WHITE + message);
+        } else if (p.hasPermission("chat.format.responsable")) {
+            event.setFormat(ChatColor.DARK_GRAY + "[" + ChatColor.BLUE + "Responsable" + ChatColor.DARK_GRAY + "] " + ChatColor.BLUE + p.getDisplayName() + ChatColor.DARK_GRAY + ": " + ChatColor.WHITE + message);
+        } else if (p.hasPermission("chat.format.builder")) {
+            event.setFormat(ChatColor.DARK_GRAY + "[" + ChatColor.GOLD + "Builder" + ChatColor.DARK_GRAY + "] " + ChatColor.GOLD + p.getDisplayName() + ChatColor.DARK_GRAY + ": " + ChatColor.WHITE + message);
+        } else {
+            event.setFormat(ChatColor.DARK_GRAY + "[" + ChatColor.GRAY + "UNKNOWN" + ChatColor.DARK_GRAY + "] " + ChatColor.GRAY + p.getDisplayName() + ChatColor.DARK_GRAY + ": " + ChatColor.WHITE + message);
         }
     }
 }
